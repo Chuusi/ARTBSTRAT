3e5e7a9db667330232bc99f6f0d79adc264f708d
Chuusi <leal.araya.angel.tic@gmail.com>
2024-05-09 20:47:19 +0200

    Preparación de archivos del front, archivo de contexto hecho y componentes básicos hechos

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,10 +1,16 @@
+import { Outlet } from 'react-router-dom'
 import './App.css'
+import { Footer, Header } from './components/'
 
 function App() {
 
   return (
     <>
-
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      < Footer />
     </>
   )
 }
---
+++ front/src/pages/ChangeForgottenPassword.jsx
@@ -0,0 +1,1 @@
+import "./ChangeForgottenPassword.css"
