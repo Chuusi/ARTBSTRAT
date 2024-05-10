6eada205ec40d0f107874688e9b30d541a3d6431
ElenatinaRuano <ruano.herrera.elena.tic@gmail.com>
2024-05-10 15:30:24 +0200

    LLamada general a la API --> service.ApiUser + updateToken

---
+++ front/src/services/service.ApiUser.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { updateToken } from '../utils';
+
+
+// Las headers de nuestro servicio
+const APIHeaders = {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    Authorization: `Bearer ${updateToken()}`,
+};
+
+// Llamamos al servicio --> url del backend
+export const APIuser = axios.create({
+    //Este puerto lo sacamos del proyecto de backend del modulo 2
+    baseURL: `http://localhost:8080/artbstrat`,
+    headers: APIHeaders,
+    timeout: 6000,
+});
