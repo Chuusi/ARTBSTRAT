fb49ded9564f2b3890c7f6cb60dbe7076a298b87
ElenatinaRuano <ruano.herrera.elena.tic@gmail.com>
2024-05-13 21:14:30 +0200

    Footer

--- front/src/components/Footer.jsx
+++ front/src/components/Footer.jsx
@@ -1,7 +1,43 @@
+import { NavLink } from "react-router-dom";
 import "./Footer.css"
 
 export const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer>
+            <div className="footer-logo-div"><img className="footer-logo" src="https://res.cloudinary.com/da7unrk9q/image/upload/v1715626802/artbstrat-removebg-preview_g67rwb.png" alt="logo" /></div>
+            <div className="footer">
+            
+                <div className="footer-div-info">
+                    
+                    <div className="footer-aboutUs">
+                        <h3 className="footer-h3">Sobre nosotros</h3>
+                        <NavLink className="footer-link" to="/aboutUs">
+                            <p className="footer-link">Proyecto Artbstrat</p>
+                        </NavLink>
+                    </div>
+
+                    <div className="footer-termsAndConditions">
+                        <h3 className="footer-h3">Información</h3>
+                        <NavLink className="footer-link" to="/termsAndConditions">
+                            <p className="footer-link">Términos y condiciones</p>
+                        </NavLink>
+                    </div>
+                    
+                    <div className="footer-help">
+                        <h3 className="footer-h3">¿Necesitas ayuda?</h3>
+                        <div className="footer-mail">
+                            <p className="material-symbols-outlined">mail</p>
+                            <p>artbstrat.shop@gmail.com</p>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="footer-div-social-media">
+                    <div className="footer-div-insta"><a href="https://www.instagram.com/artbstrat/" className="footer-insta fa fa-instagram"></a></div>
+                    <div className="footer-signature">2024 © Artbstrat SL</div>
+                </div>
+            </div>
+        </footer>
     )
 }
