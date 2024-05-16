cb1c10ffe7d60675116a7d9b0f1555f5cf94a92d
ElenatinaRuano <ruano.herrera.elena.tic@gmail.com>
2024-05-16 22:32:04 +0200

    HTML + CSS shop page

--- front/src/pages/Shop.jsx
+++ front/src/pages/Shop.jsx
@@ -1,7 +1,26 @@
 import "./Shop.css"
+import { ProductCard  } from "../components/ProductCard";
+import { getAllProducts } from "../services/product.service";
+
+const productList = await getAllProducts();
 
 export const Shop = () => {
+
     return (
-        <div>Shop</div>
+        <div className="shop-page">
+            <h2>TIENDA ARTBSTRAT</h2>
+            
+            <div className="shop-container-gallery">
+                {productList.data.map((product) => 
+                    <ProductCard 
+                        id={product.id}
+                        image={product.image}
+                        name={product.name}
+                        price={product.price}
+                        offerPrice={product.offerPrice}
+                    />
+                )}
+            </div>
+        </div>
     )
 }
