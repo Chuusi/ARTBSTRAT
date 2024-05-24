2fc2febdccf8888417391c38cca44eab3438ee20
Chuusi <leal.araya.angel.tic@gmail.com>
2024-05-24 14:59:13 +0200

    terminos not found e intento de fav

--- front/src/pages/NotFound.jsx
+++ front/src/pages/NotFound.jsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom"
 import "./NotFound.css"
 
 export const NotFound = () => {
     return (
-        <div>NotFound</div>
+        <div className="notfound-container">
+            <div className="notfound-logo-container">
+                <img src="logo_artbs.png" alt="logo_artbstrat" />
+                {/* <span class="material-symbols-outlined notfound-drop">
+                water_drop
+                </span> */}
+            </div>
+            
+            <h2>Achooo, algo no ha ido bien...</h2>
+            <Link to="/"><h3>Volver la página principal</h3></Link>
+        </div>
     )
 }
