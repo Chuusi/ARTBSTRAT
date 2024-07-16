65d2912218d1ed3aa9c9760fb24ce2bc5f8c2eed
Chuusi <leal.araya.angel.tic@gmail.com>
2024-07-16 13:22:44 +0200

    Carousel de comentarios añadido y banner modificado

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -2,25 +2,62 @@ import "./Home.css"
 import { NavLink } from "react-router-dom";
 import { getAllComment } from "../services/comment.service";
 import { useEffect, useState } from "react";
+import 'bootstrap/dist/css/bootstrap.css'; 
+import Carousel from 'react-bootstrap/Carousel'; 
 
 //Traemos todos los comentarios
 const listComment = await getAllComment();
 
 export const Home = () => {
     
-    const [carruselPointer, setCarruselPointer] = useState(0);
     const [selected, setSelected] = useState([]);
+    const [index, setIndex] = useState(0)
 
     useEffect(() => {
         setSelected(listComment);
     },[listComment])
 
 
+    const handleSelect = (selectedIndex) => {
+        setIndex(selectedIndex);
+    }
+
+    const CustomPrevButton = ({index}) => {
+        const prevIndex = (index === 0 ? 4 : index - 1);
+        return (
+            <div className="custom-control-prev-icon custom-control">
+                <div className="home-carrusel-comment">
+                    <img className="home-logo-comment" src={selected?.data?.results[prevIndex]?.image} alt="https://res.cloudinary.com/da7unrk9q/image/upload/v1715797954/logo_wqxsk8.jpg" />
+                    <div className="home-div-comment">
+                        <h3 className="home-username">{selected?.data?.results[prevIndex]?.comment?.ownerName}</h3>
+                        <p className="home-p-comment">{selected?.data?.results[prevIndex]?.comment?.content}</p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    const CustomNextButton = ({index}) => {
+        const nextIndex = (index === 4 ? 0 : index + 1);
+        return (
+            <div className="custom-control-next-icon custom-control">
+                <div className="home-carrusel-comment">
+                    <img className="home-logo-comment" src={selected?.data?.results[nextIndex]?.image} alt="https://res.cloudinary.com/da7unrk9q/image/upload/v1715797954/logo_wqxsk8.jpg" />
+                    <div className="home-div-comment">
+                        <h3 className="home-username">{selected?.data?.results[nextIndex]?.comment?.ownerName}</h3>
+                        <p className="home-p-comment">{selected?.data?.results[nextIndex]?.comment?.content}</p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
     return (
         <div className="home-page">
             <div className="home-banner">
-                <video className="home-banner-video" src="../../public/bannerBeach.mp4" autoPlay loop muted/>
-                <img className="home-banner-logo" src="https://res.cloudinary.com/da7unrk9q/image/upload/v1715626802/artbstrat-removebg-preview_g67rwb.png" alt="logoArtbstrat" />
+                <video className="home-banner-video" src="/bannerBeach.mp4" autoPlay loop muted/>
+                <h1 className="home-banner-logo">ARTBSTRAT</h1>
+                {/*<img className="home-banner-logo" src="https://res.cloudinary.com/da7unrk9q/image/upload/v1715626802/artbstrat-removebg-preview_g67rwb.png" alt="logoArtbstrat" />*/}
             </div>
 
             <div className="home-buttoms">
@@ -47,43 +84,34 @@ export const Home = () => {
                 <h3 className="home-comunity-h3">NUESTRA COMUNIDAD</h3>
 
                 {/**CARRUSEL DE COMENTARIOS*/}
-                    <div className="home-carrusel">
-                        <div id="contentItemsCarrusel" className="home-carrusel-content">
-                            {selected?.data?.results.map((element, index) => {
-                                return(
-                                <div key={index} className="home-carrusel-page" id={index}>
-                                    <div className="home-carrusel-comment">
-                                    <img className="home-logo-comment" src={element?.image} alt="https://res.cloudinary.com/da7unrk9q/image/upload/v1715797954/logo_wqxsk8.jpg" />
-                                        <div className="home-div-comment">
-                                            <h3 className="home-username">{element?.comment.ownerName}</h3>
-                                            <p className="home-p-comment">{element?.comment.content}</p>
-                                        </div>
-                                    </div>
-                                    <div className="home-carrusel-direction" >
-            
-                                        <button 
-                                            className ="home-carrusel-buttoms" 
-                                            onClick={() => carruselPointer == 0 ? "" : setCarruselPointer(carruselPointer - 1)}
-                                            style={{opacity:carruselPointer == 0 ? "0%" : "100%"}}
-                                        ><a href={`#${index-1}`}><span className="home-arrow material-symbols-outlined left" >keyboard_double_arrow_left</span></a></button>
-                                        <button 
-                                            className ="home-carrusel-buttoms" 
-                                            onClick={() => setCarruselPointer(carruselPointer + 1)}
-                                            style={{display:carruselPointer == 4 ? "none" : "initial"}}
-                                        ><a href={`#${index+1}`}><span className="home-arrow material-symbols-outlined right">keyboard_double_arrow_right</span></a></button>
-                                    </div>
-                                </div>)
-                            })}
-                        </div>
 
-                        <div className="home-div-points">
-                            {selected?.data?.results.map((comment, index) => 
-                                    <div key={index} >
-                                        <span id={index} className="home-points material-symbols-outlined">radio_button_{carruselPointer==index ? "checked" : "unchecked"}</span>
-                                    </div>
-                            )}
-                    </div>
-                </div>
+                {/**CARRUSEL DE COMENTARIOS CON BOOTSTRAP*/}
+                <Carousel
+                    className='my-carousel'
+                    indicators={false}
+                    pause='hover'
+                    interval={3000}
+                    onSelect={handleSelect}
+                    activeIndex={index}
+                    prevIcon={<CustomPrevButton index={index}></CustomPrevButton>}
+                    nextIcon={<CustomNextButton index={index}></CustomNextButton>}
+                    autoPlay
+                    autoPlaySpeed={1}
+                >
+                    {selected?.data?.results.map((element, i) => (
+                        <Carousel.Item
+                            key={i}
+                        >
+                            <div className="home-carrusel-comment">
+                                <img className="home-logo-comment" src={element?.image} alt="https://res.cloudinary.com/da7unrk9q/image/upload/v1715797954/logo_wqxsk8.jpg" />
+                                <div className="home-div-comment">
+                                    <h3 className="home-username">{element?.comment.ownerName}</h3>
+                                    <p className="home-p-comment">{element?.comment.content}</p>
+                                </div>
+                            </div>
+                        </Carousel.Item>
+                    ))}
+                </Carousel>
             </div>
         </div>
     )
